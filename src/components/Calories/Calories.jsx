085090d75b3c3893acc9284243e82f25790e7a62
JaRoD41 <laurent.trinco@gmail.com>
2023-06-22 16:52:55 +0200

    add calories component

--- src/components/Calories/Calories.jsx
+++ src/components/Calories/Calories.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import calories from '../../assets/calory-icon.svg'
 
 export default function Calories() {
+  const calorieCount = 1930
   return (
-    <div className='name'>Calories</div>
+    <article className='nutrition_card'>
+      <div className="calory_button">
+        <img src={calories} alt="calories" />
+      </div>
+      <div className="calory_content">
+        <h3>{calorieCount}kCal</h3>
+        <span>Calories</span>
+      </div>
+    </article>
   )
 }
--- src/components/NutritionZone/NutritionZone.jsx
+++ src/components/NutritionZone/NutritionZone.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Calories from '../Calories/Calories'
+import Proteins from '../Proteins/Proteins'
+import Glucids from '../Glucids/Glucids'
+import Lipids from '../Lipids/Lipids'
 
 export default function NutritionZone() {
   return (
-    <div>NutritionZone</div>
-  )
+		<aside className="nutrition_container">
+			<Calories />
+			<Proteins />
+			<Glucids />
+			<Lipids />
+		</aside>
+	)
 }
