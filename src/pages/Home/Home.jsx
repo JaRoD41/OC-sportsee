616c4b2bc70a5ea8a8fca0564b1bc0651f58775a
JaRoD41 <laurent.trinco@gmail.com>
2023-06-22 10:29:09 +0200

    start style vertical nav

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,8 +5,8 @@ import HorizontalNav from '../../components/HorizontalNav/HorizontalNav'
 const Home = () => {
 	return (
 		<>
-			<VerticalNav />
 			<HorizontalNav />
+			<VerticalNav />
 		</>
 	)
 }
