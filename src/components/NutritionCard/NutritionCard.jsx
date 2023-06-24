0877818f5cbf57bc10f3641f859497a2d6416d27
JaRoD41 <laurent.trinco@gmail.com>
2023-06-24 17:34:37 +0200

    create nutrition card

---
+++ src/components/NutritionCard/NutritionCard.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import calory from '../../assets/calory-icon.svg'
+import protein from '../../assets/protein-icon.svg'
+import glucid from '../../assets/glucid-icon.svg'
+import lipid from '../../assets/lipid-icon.svg'
+
+export default function NutritionCard({ type, amount }) {
+	let icon = ''
+	switch (type) {
+		case 'calories':
+			icon = calory
+			break
+		case 'proteins':
+			icon = protein
+			break
+		case 'glucids':
+			icon = glucid
+			break
+		case 'lipids':
+			icon = lipid
+			break
+		default:
+			break
+	}
+	return (
+		<article className="nutrition-card">
+			<div className="nutrition-card_button">
+				<img src={icon} alt={type} />
+			</div>
+			<div className="nutrition-card_content">
+				<h3>{amount}kCal</h3>
+				<span>Lipides</span>
+			</div>
+		</article>
+	)
+}
