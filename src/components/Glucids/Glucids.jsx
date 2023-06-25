9cf5333782ab71da00242b63488f316c828ab6de
JaRoD41 <laurent.trinco@gmail.com>
2023-06-25 18:54:04 +0200

    fix icon color in a useEffect

--- src/components/Glucids/Glucids.jsx
+++ src/components/Glucids/Glucids.jsx
@@ -2,16 +2,9 @@ import React from 'react'
 import glucids from '../../assets/glucid-icon.svg'
 
 export default function Glucids() {
-  const carbohydrateCount = 290
 	return (
-		<article className="nutrition-card">
-			<div className="nutrition-card_button button-yellow">
-				<img src={glucids} alt="glucids" />
-			</div>
-			<div className="nutrition-card_content">
-				<h3>{carbohydrateCount}kCal</h3>
-				<span>Glucides</span>
-			</div>
-		</article>
+		<div className="nutrition-card-button">
+			<img src={glucids} alt="glucids" />
+		</div>
 	)
 }
--- src/components/NutritionCard/NutritionCard.jsx
+++ src/components/NutritionCard/NutritionCard.jsx
@@ -1,32 +1,45 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import calory from '../../assets/calory-icon.svg'
 import protein from '../../assets/protein-icon.svg'
 import glucid from '../../assets/glucid-icon.svg'
 import lipid from '../../assets/lipid-icon.svg'
 
+
+// Je récupère les props type et amount de NutritionZone.jsx
 export default function NutritionCard({ type, amount }) {
+	// Je déclare une variable d'état color grâce à useState
+	const [color, setColor] = useState('')
+
 	let icon = ''
-	switch (type) {
-		case 'calories':
-			icon = calory
-			break
-		case 'proteins':
-			icon = protein
-			break
-		case 'glucids':
-			icon = glucid
-			break
-		case 'lipids':
-			icon = lipid
-			break
-		default:
-			break
-	}
+	useEffect(() => {
+		switch (type) {
+			// Selon le type de la carte, je change la couleur de l'icone
+			case 'calories':
+				setColor('red')
+				// icon = calory
+				break
+			case 'proteins':
+				setColor('blue')
+				// icon = protein
+				break
+			case 'glucids':
+				setColor('yellow')
+				// icon = glucid
+				break
+			case 'lipids':
+				setColor('pink')
+				// icon = lipid
+				break
+			default:
+				break
+		}
+		// eslint-disable-next-line
+	}, [type])
 	return (
 		<article className="nutrition-card">
-			<div className='nutrition-card-infos'>
-				<div className="nutrition-card-infos__button">
-					<img src={icon} alt={type} />
+			<div className="nutrition-card-infos">
+				<div className={`nutrition-card-infos__button ${color}`}>
+					<img src={calory} alt={type} />
 				</div>
 				<div className="nutrition-card-infos__content">
 					<h3>{amount}kCal</h3>
