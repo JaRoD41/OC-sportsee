25b871fb62c2b67863546a49f7ca8ac8a5883887
JaRoD41 <laurent.trinco@gmail.com>
2023-06-27 15:38:19 +0200

    add copyright

--- src/components/VerticalNav/VerticalNav.jsx
+++ src/components/VerticalNav/VerticalNav.jsx
@@ -3,8 +3,9 @@ import VerticalNavLinks from '../VerticalNavLinks/VerticalNavLinks'
 
 export default function VerticalNav() {
   return (
-    <aside className='verticalContainer'>
-      <VerticalNavLinks />
-    </aside>
-  )
+		<aside className="verticalContainer">
+			<VerticalNavLinks />
+			<span className="copyright">Copyright, SportSee 2023</span>
+		</aside>
+	)
 }
