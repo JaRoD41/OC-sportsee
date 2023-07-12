9cda8e65736f9cca1c38708f621d3d651b94024a
JaRoD41 <laurent.trinco@gmail.com>
2023-07-12 11:25:17 +0200

    tests radar style

--- src/components/Radar/Radar.jsx
+++ src/components/Radar/Radar.jsx
@@ -1,65 +1,59 @@
 import React from 'react'
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts'
-import { getPerformanceDataValue, getPerformanceKind } from '../../services/PerformanceModel'
+import { getPerformanceDataValue } from '../../services/PerformanceModel'
 let data = []
 
-
-
-export default function RadarChartComponent({ userId, performanceDataAll }) {
-	let user = userId
-  let kind = performanceDataAll.kind
-	console.log('performanceDataAll dans radar:', performanceDataAll)
-	// console.log('performanceKind :', performanceKind)
-
-	// let performanceKindArray = Object.values(performanceKind)
-	// let performanceDataArray = Array.from(performanceDataAll)
+export default function RadarChartComponent({ performanceDataAll }) {
 	data = getPerformanceDataValue(performanceDataAll)
-	// kind = getPerformanceKind(performanceDataAll)
-	// console.log('data dans radar après fonction :', data)
-	// let kindArray = Array.from(kind)
-
 	console.log('data dans radar après fonction :', data)
 
 	// const data = [
 	// 	{
 	// 		subject: 'Math',
 	// 		A: 120,
+	// 		B: 110,
 	// 		fullMark: 150,
 	// 	},
 	// 	{
 	// 		subject: 'Chinese',
 	// 		A: 98,
+	// 		B: 130,
 	// 		fullMark: 150,
 	// 	},
 	// 	{
 	// 		subject: 'English',
 	// 		A: 86,
+	// 		B: 130,
 	// 		fullMark: 150,
 	// 	},
 	// 	{
 	// 		subject: 'Geography',
 	// 		A: 99,
+	// 		B: 100,
 	// 		fullMark: 150,
 	// 	},
 	// 	{
 	// 		subject: 'Physics',
 	// 		A: 85,
+	// 		B: 90,
 	// 		fullMark: 150,
 	// 	},
 	// 	{
 	// 		subject: 'History',
 	// 		A: 65,
+	// 		B: 85,
 	// 		fullMark: 150,
 	// 	},
 	// ]
+
 	return (
 		<div className="radar-container">
 			<ResponsiveContainer width="100%" height="100%">
 				<RadarChart cx="50%" cy="50%" outerRadius="80%" data={data}>
 					<PolarGrid />
-					<PolarAngleAxis dataKey="kind" />
-					<PolarRadiusAxis />
-					<Radar dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+					<PolarAngleAxis dataKey="kind" stroke='#FFF'/>
+					{/* <PolarRadiusAxis /> */}
+					<Radar dataKey="A" stroke="#8884d8" fill="#FF0101" fillOpacity={0.7} dot={false} />
 				</RadarChart>
 			</ResponsiveContainer>
 		</div>
