8827ac4403bd4ad0caaffb58ca2e82fc8b98099b
JaRoD41 <laurent.trinco@gmail.com>
2023-07-13 12:30:55 +0200

    add style to labels using nth child

--- src/components/Radar/Radar.jsx
+++ src/components/Radar/Radar.jsx
@@ -7,22 +7,22 @@ export default function RadarChartComponent({ performanceDataAll }) {
 	data = getPerformanceDataValue(performanceDataAll)
 	console.log('data dans radar après fonction :', data)
 
-	// Je crée une fonction getKind qui va me permettre de récupérer les valeurs de kind dans data et les traduire en français pour le graphique
+	// Je crée une fonction getKind qui va me permettre de récupérer les valeurs de kind dans data, les traduire en français pour le graphique et mettre dans le bon ordre pour le graphique
 	const getKind = (data) => {
 		let kind = data.kind
 		switch (kind) {
 			case 'cardio':
-				return 'Cardio'
-			case 'energy':
-				return 'Energie'
-			case 'endurance':
-				return 'Endurance'
-			case 'strength':
-				return 'Force'
-			case 'speed':
-				return 'Vitesse'
-			case 'intensity':
 				return 'Intensité'
+			case 'energy':
+				return 'Vitesse'
+			case 'endurance':
+				return 'Force'
+			case 'strength':
+				return 'Endurance'
+			case 'speed':
+				return 'Energie'
+			case 'intensity':
+				return 'Cardio'
 			default:
 				return 'Valeur inconnue'
 		}
@@ -31,10 +31,10 @@ export default function RadarChartComponent({ performanceDataAll }) {
 	return (
 		<div className="radar-container">
 			<ResponsiveContainer width="100%" height="100%">
-				<RadarChart outerRadius="80%" data={data} margin={{ top: 5, right: 5, bottom: 5, left: 5 }}>
-					<PolarGrid radialLines={false} polarRadius={[0, 10, 27, 49, 72, 95]} />
+				<RadarChart outerRadius="80%" data={data} margin={{ top: 25, right: 25, bottom: 25, left: 25 }}>
+					<PolarGrid radialLines={false}  />
 					<PolarAngleAxis dataKey={getKind} tick={{ fill: 'white', fontSize: 12 }} />
-					<Radar dataKey="A" fill="#FF0101" fillOpacity={0.7} dot={false} />
+					<Radar dataKey="A" fill="#FF0101" fillOpacity={0.7} dot={false} animationEasing='ease-in-out'/>
 				</RadarChart>
 			</ResponsiveContainer>
 		</div>
