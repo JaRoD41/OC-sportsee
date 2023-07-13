dc3676fde746f8691b042cb43ec5d145068284e5
JaRoD41 <laurent.trinco@gmail.com>
2023-07-13 10:55:54 +0200

    add translate function for kind

--- src/components/Radar/Radar.jsx
+++ src/components/Radar/Radar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts'
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from 'recharts'
 import { getPerformanceDataValue } from '../../services/PerformanceModel'
 let data = []
 
@@ -7,52 +7,33 @@ export default function RadarChartComponent({ performanceDataAll }) {
 	data = getPerformanceDataValue(performanceDataAll)
 	console.log('data dans radar après fonction :', data)
 
-	// const data = [
-	// 	{
-	// 		subject: 'Math',
-	// 		A: 120,
-	// 		B: 110,
-	// 		fullMark: 150,
-	// 	},
-	// 	{
-	// 		subject: 'Chinese',
-	// 		A: 98,
-	// 		B: 130,
-	// 		fullMark: 150,
-	// 	},
-	// 	{
-	// 		subject: 'English',
-	// 		A: 86,
-	// 		B: 130,
-	// 		fullMark: 150,
-	// 	},
-	// 	{
-	// 		subject: 'Geography',
-	// 		A: 99,
-	// 		B: 100,
-	// 		fullMark: 150,
-	// 	},
-	// 	{
-	// 		subject: 'Physics',
-	// 		A: 85,
-	// 		B: 90,
-	// 		fullMark: 150,
-	// 	},
-	// 	{
-	// 		subject: 'History',
-	// 		A: 65,
-	// 		B: 85,
-	// 		fullMark: 150,
-	// 	},
-	// ]
+	// Je crée une fonction getKind qui va me permettre de récupérer les valeurs de kind dans data et les traduire en français pour le graphique
+	const getKind = (data) => {
+		let kind = data.kind
+		switch (kind) {
+			case 'cardio':
+				return 'Cardio'
+			case 'energy':
+				return 'Energie'
+			case 'endurance':
+				return 'Endurance'
+			case 'strength':
+				return 'Force'
+			case 'speed':
+				return 'Vitesse'
+			case 'intensity':
+				return 'Intensité'
+			default:
+				return 'Valeur inconnue'
+		}
+	}
 
 	return (
 		<div className="radar-container">
 			<ResponsiveContainer width="100%" height="100%">
-				<RadarChart outerRadius="75%" data={data} margin={{ top: 5, right: 5, bottom: 5, left: 5 }}>
+				<RadarChart outerRadius="80%" data={data} margin={{ top: 5, right: 5, bottom: 5, left: 5 }}>
 					<PolarGrid radialLines={false} polarRadius={[0, 10, 27, 49, 72, 95]} />
-					<PolarAngleAxis dataKey="kind" tick={{ fill: 'white', fontSize: 12 }} />
-					{/* <PolarRadiusAxis /> */}
+					<PolarAngleAxis dataKey={getKind} tick={{ fill: 'white', fontSize: 12 }} />
 					<Radar dataKey="A" fill="#FF0101" fillOpacity={0.7} dot={false} />
 				</RadarChart>
 			</ResponsiveContainer>
