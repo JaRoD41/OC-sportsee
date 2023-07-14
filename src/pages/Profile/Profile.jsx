035ec79b7ba4b5564c42145a5d8669717523c384
JaRoD41 <laurent.trinco@gmail.com>
2023-07-14 19:37:01 +0200

    add user logo to selection page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,26 +1,35 @@
 import { useNavigate } from 'react-router-dom'
 import VerticalNav from '../../components/VerticalNav/VerticalNav'
 import HorizontalNav from '../../components/HorizontalNav/HorizontalNav'
+import logo from '../../assets/SportSee_user_logo.png'
 
 const Profile = () => {
 	const navigate = useNavigate()
 
 	return (
-		<div className='userSelection-container'>
+		<div className="userSelection-container">
 			<HorizontalNav />
 			<VerticalNav />
 			<nav className="userSelection">
 				<h1 className="userSelection__title">Choix de l'utilisateur</h1>
 				<ul className="userSelection__list">
 					<li className="userSelection__list-item">
-						<button className="userSelection__list-item-btn" onClick={() => navigate('/user/12')}>
-							Karl
-						</button>
+						<span className="userName">Karl</span>
+						<img
+							className="userSelection__list-item-btn"
+							src={logo}
+							alt="user logo"
+							onClick={() => navigate('/user/12')}
+						/>
 					</li>
 					<li className="userSelection__list-item">
-						<button className="userSelection__list-item-btn" onClick={() => navigate('/user/18')}>
-							Cecilia
-						</button>
+						<span className="userName">Cecilia</span>
+						<img
+							src={logo}
+							alt="user logo"
+							className="userSelection__list-item-btn"
+							onClick={() => navigate('/user/18')}
+						/>
 					</li>
 				</ul>
 			</nav>
