28c09a6874408d041c9ea10c8041d1816d095321
JaRoD41 <laurent.trinco@gmail.com>
2023-07-24 14:25:14 +0200

    typo + fix links

--- src/components/HorizontalNavLinks/HorizontalNavLinks.jsx
+++ src/components/HorizontalNavLinks/HorizontalNavLinks.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom'
 
 export default function HorizontalNavLinks() {
-	let horizontal_links = ['Accueil', 'Profil', 'Réglage', 'Communauté']
+	// Je crée un tableau qui contient les liens de la navigation horizontale
+	let horizontal_links = ['Accueil', 'Profil', 'Réglages', 'Communauté']
 	return (
 		<nav className="horizontal_links">
 			{horizontal_links.map((link, index) => (
-				<Link key={index} to="/">
+				<Link key={index} to="/user/:userId">
 					{link}
 				</Link>
 			))}
--- src/components/VerticalNavLinks/VerticalNavLinks.jsx
+++ src/components/VerticalNavLinks/VerticalNavLinks.jsx
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom'
 import ActivityButton from '../ActivityButton/ActivityButton'
 
 export default function VerticalNavLinks() {
+	// Je crée un tableau qui contient les liens de la navigation verticale
 	let activities = ['zen', 'swim', 'bike', 'weight']
 	return (
 		<nav className="verticalButtons-container">
 			{activities.map((activity, index) => {
 				return (
-					<Link key={index} to="/">
+					<Link key={index} to="/user/:userId">
 						<ActivityButton key={index} activity={activity} />
 					</Link>
 				)
