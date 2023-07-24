e89fdef13d7e13e756e5d03d029a2faedfc65182
JaRoD41 <laurent.trinco@gmail.com>
2023-07-24 16:11:50 +0200

    modify nutrition card hover effect

--- src/components/NutritionCard/NutritionCard.jsx
+++ src/components/NutritionCard/NutritionCard.jsx
@@ -4,11 +4,30 @@ import glucid from '../../assets/glucid-icon.svg'
 import lipid from '../../assets/lipid-icon.svg'
 
 // Je crée un objet TYPES qui contient les propriétés color, unit, typeName et icon qui seront utilisées dans NutritionCard.jsx
+// const TYPES = {
+// 	calorieCount: { color: 'red', unit: 'kCal', typeName: 'Calories', icon: calory },
+// 	proteinCount: { color: 'blue', unit: 'g', typeName: 'Protéines', icon: protein },
+// 	carbohydrateCount: { color: 'yellow', unit: 'g', typeName: 'Glucides', icon: glucid },
+// 	lipidCount: { color: 'pink', unit: 'g', typeName: 'Lipides', icon: lipid },
+// }
+
 const TYPES = {
-	calorieCount: { color: 'red', unit: 'kCal', typeName: 'Calories', icon: calory },
-	proteinCount: { color: 'blue', unit: 'g', typeName: 'Protéines', icon: protein },
-	carbohydrateCount: { color: 'yellow', unit: 'g', typeName: 'Glucides', icon: glucid },
-	lipidCount: { color: 'pink', unit: 'g', typeName: 'Lipides', icon: lipid },
+	calorieCount: { color: 'red', unit: 'kCal', typeName: 'Calories', icon: calory, hoverColor: 'rgba(255, 0, 0, 0.4)' },
+	proteinCount: {
+		color: 'blue',
+		unit: 'g',
+		typeName: 'Protéines',
+		icon: protein,
+		hoverColor: 'rgba(74, 184, 255, 0.4)',
+	},
+	carbohydrateCount: {
+		color: 'yellow',
+		unit: 'g',
+		typeName: 'Glucides',
+		icon: glucid,
+		hoverColor: 'rgba(249, 206, 35, 0.4)',
+	},
+	lipidCount: { color: 'pink', unit: 'g', typeName: 'Lipides', icon: lipid, hoverColor: 'rgba(253, 81, 129, 0.4)' },
 }
 
 // Je récupère les props type et amount de NutritionZone.jsx
@@ -17,7 +36,8 @@ export default function NutritionCard({ type, amount }) {
 	const nutritionType = TYPES[type]
 
 	return (
-		<article className="nutrition-card">
+		// Je crée une carte qui affiche le type de nutrition, la quantité et l'unité et modifie la couleur de fond en fonction du type de nutrition
+		<article className="nutrition-card" style={{ '--hover-color': nutritionType.hoverColor }}>
 			<div className="nutrition-card-infos">
 				<div className={`nutrition-card-infos__button ${nutritionType.color}`}>
 					<img src={nutritionType.icon} alt={nutritionType} />
