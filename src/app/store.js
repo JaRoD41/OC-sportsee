5e13f050f4255d095403d5552e3d6d2c4e97d765
JaRoD41 <laurent.trinco@gmail.com>
2023-08-11 17:23:58 +0200

    add comment

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    // Je dois ajouter un reducer pour que le store fonctionne
+  },
 });
